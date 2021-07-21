62702543c2c560ff2919ae9addcb929fbfa5b746
sangwoo0727 <kangsw9395@gmail.com>
2021-07-22 04:31:49 +0900

    add shallow etag header filter

---
+++ src/main/java/io/hala/etagtemplate/config/CacheConfig.java
@@ -0,0 +1,18 @@
+package io.hala.etagtemplate.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class CacheConfig {
+
+  @Bean
+  public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+    FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(
+        new ShallowEtagHeaderFilter());
+    filterRegistrationBean.addUrlPatterns("/book/cache");
+    return filterRegistrationBean;
+  }
+}
