67dcdc3f2949371b6d9324b675b1ddd25d8898f0
sangwoo0727 <kangsw9395@gmail.com>
2021-07-22 02:45:31 +0900

    init project

---
+++ src/test/java/io/hala/etagtemplate/EtagTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hala.etagtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtagTemplateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
